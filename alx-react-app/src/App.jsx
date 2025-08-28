0c10e26fa5d7c05fdd6b24195b68dd40d2faf38a
Yerosen Bekele <yerobeke@gmail.com>
2025-08-28 18:59:19 +0300

    add

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,16 +1,15 @@
 
 import WelcomeMessage from './components/WelcomeMessage'
 import Header from './components/Header.jsx'
-import MainContent from './components/MainContent.jsx'
 import Footer from './components/Footer.jsx'
-
+import Maincontent from './components/MainContent.jsx'
 
 function App() {
 
   return (
     <>
        <Header />
-        <MainContent />
+        <Maincontent />
         <Footer />
       <WelcomeMessage />
     </>
