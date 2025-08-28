c776946af5a29d8c3f865fdd2ddc1599f039eea7
Yerosen Bekele <yerobeke@gmail.com>
2025-08-28 23:00:58 +0300

    add

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,15 +3,22 @@ import Header from './components/Header.jsx'
 import MainContent from './components/MainContent.jsx'
 import Footer from './components/Footer.jsx'
 import WelcomeMessage from './components/WelcomeMessage'
+import UserProfile from './components/UserProfile.jsx'
 
 function App() {
 
   return (
     <>
-       <Header />
-        <MainContent />
-        <Footer />
+      <Header />
+      <MainContent />
+      <UserProfile
+        name="Yerosen"
+        age={30}
+        bio="Software Developer"
+      />
+      <Footer />
       <WelcomeMessage />
+
     </>
   )
 }
