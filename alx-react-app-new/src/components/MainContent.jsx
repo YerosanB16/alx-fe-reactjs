ae62fc2b5dfe31421bcfcbe460a42e9f5d0f94cd
Yerosen Bekele <yerobeke@gmail.com>
2025-09-06 10:57:27 +0300

    add

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,11 @@
+
+function MainContent() {
+  return (
+    <>
+      <main style={{ backgroundColor: 'navy', color: 'white', textAlign: 'center' }}>>
+        <p style={{ fontStyle: 'italic' }}>I love to visit New York, Paris, and Tokyo.</p>
+      </main>
+    </>
+  )
+}
+export default MainContent;
